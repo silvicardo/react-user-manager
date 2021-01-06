ac2d47e32eb05f89eef3b822978364924a39f4c6
silvicardo <silvicardo@gmail.com>
2021-01-06 11:16:53 +0100

    users list success tests

--- cypress/integration/pages/UsersList.spec.js
+++ cypress/integration/pages/UsersList.spec.js
@@ -1,17 +1,50 @@
+import lang from "../../../src/lang";
+import faker from 'faker';
 
 describe('USERS LIST PAGE',function(){
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
 
     it('initial load', function(){
 
         it('is reachable', function(){
 
+            cy.url().should('include', '/');
+
+        })
+
+        it('shows page title', function(){
+
+            cy.findByText(lang.users.list).should('exist').and('be.visible');
+
+        })
+
+        it('shows add new user button', function(){
+
+            cy.findByText(lang.users.actions.add).should('exist').and('be.visible');
+
         })
 
         it('shows a non empty list of users', function(){
 
+            const fakeUsersList = new Array(10).map((_, idx) => ({id: idx,  name: faker.fake("{{name.lastName}}} {{name.firstName}}") }));
+
+            cy.intercept('**/users', fakeUsersList).as('allUse');
+
+            fakeUsersList.forEach(user => {
+                cy.findByText(user.name).should('exist').and('be.visible');
+            })
+
         })
 
         it('shows a message on empty users response', function(){
+
+            cy.intercept('**/users', []).as('emptyUsers');
+
+            //TODO:: create copy in branch lang-text
+            cy.findByText('no users found').should('exist').and('be.visible');
 
         })
 
