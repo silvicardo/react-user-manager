7fee339987af2cad1f4de8b5833aa492d6568e51
silvicardo <silvicardo@gmail.com>
2021-01-06 10:20:45 +0100

    lang file

---
+++ src/lang/index.js
@@ -0,0 +1,16 @@
+const lang = {
+    users : {
+        new: 'new user',
+        list: 'users',
+        name: 'name',
+        friends: 'friends',
+        actions : {
+            remove: 'remove',
+            add : 'new',
+            select : 'select friend',
+            create: 'save',
+        }
+    }
+}
+
+export default lang;
