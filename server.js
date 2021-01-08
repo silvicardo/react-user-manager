1e08837aec931e60ac66e52c8d4cc4644946af1e
silvicardo <silvicardo@gmail.com>
2021-01-08 21:53:13 +0100

    user friends related routes

--- server.js
+++ server.js
@@ -10,12 +10,24 @@ const API_URL = `http://localhost:${PORT}`;
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
 
-server.use((req, res, next) => {
+//GET - USER -> FRIENDS
 
-    if(req.method === 'PUT'){
-        req.body.updatedAt = Date.now();
+server.get('/user/:userid/friends', async (req, res) => {
+
+    try {
+
+        const response = await axios.get(`${API_URL}/friendships?_expand=user&ownerId=${req.params.userid}`);
+
+        return res.jsonp(response.data.map(friendship => ({
+            friendshipId: friendship.id,
+            ownerId: friendship.ownerId,
+            ...friendship.user
+        })));
+
+    } catch (e) {
+        return res.status(500).jsonp({ error: e, message: e.message})
     }
-    next();
+
 })
 
 //GET - USER -> NOT FRIENDS
@@ -33,11 +45,11 @@ server.get('/user/:userid/not-friends', async (req, res) => {
 
         const notFriendsResponse = await axios.get(`${API_URL}/users?${excludedUsersIdQueryString}`);
 
-        res.jsonp(notFriendsResponse.data);
+        return res.jsonp(notFriendsResponse.data);
 
     } catch(e){
 
-        res.status(500).jsonp({ error: e})
+        return res.status(500).jsonp({ error: e})
     }
 
 })
@@ -49,50 +61,64 @@ server.post('/user/create', async (req, res) => {
         const {data} = await axios.get(`${API_URL}/users?name_like=${req.body.name}`);
 
         if(data.length > 0){
-            res.status(403).jsonp({ error: 'Duplicates not permitted'});
-        } else {
-            req.body.createdAt = Date.now();
-            req.body.createdAt = Date.now();
-            const createdUserResponse = await axios.post(`${API_URL}/users`, req.body);
-            res.jsonp(createdUserResponse.data);
+            return res.status(403).jsonp({ error: 'Duplicates not permitted'});
         }
 
+        req.body.createdAt = Date.now();
+        req.body.updatedAt = Date.now();
+
+        const createdUserResponse = await axios.post(`${API_URL}/users`, req.body);
+
+        return res.jsonp(createdUserResponse.data);
+
     } catch (e){
-        res.status(500).jsonp({ error: e})
+        return res.status(500).jsonp({ error: e})
     }
 })
 
+//PUT - USER EDIT (DUPLICATE CHECK)
+server.put('/user/:userid/edit', async (req, res) => {
+
+    const {userid} = req.params;
+    const {name} = req.body;
+
+    try {
+        const userByIdResponse = await axios.get(`${API_URL}/users/${userid}`);
+
+        console.log(userByIdResponse.data)
+
+        if(!userByIdResponse.data.id){
+          return res.status(400).jsonp({ error: 'Attempting to edit not existing user'});
+        }
+
+        const userWithSameNameResponse = await axios.get(`${API_URL}/users?name_like=${name}&id_ne=${userid}`);
+
+        if(userWithSameNameResponse.data.length > 0) {
+            return res.status(403).jsonp({error: 'Duplicates not permitted'});
+        }
+
+        const userUpdates = {
+            name,
+            createdAt: userByIdResponse.data.createdAt,
+            updatedAt: Date.now()
+        }
+
+        const createdUserResponse = await axios.put(`${API_URL}/users/${userid}`, userUpdates);
+
+        return res.jsonp(createdUserResponse.data);
+
+
+    } catch (e){
+        return res.status(500).jsonp({ error: e})
+    }
+})
+
+//DELETE - FRIENDSHIP (regular json server -> friendships/5 )
+
 
 server.use(jsonServer.rewriter({
-    '/user/:userid' : '/user/:userid',
-    '/user/:userid/friends': `/friendships?_expand=user&ownerId=:userid`,
-    '/user/:userid/update' : `/users/:userid`,
     '/user/:userid/unfriend/:friendid': `/friendships?_expand=user&ownerId=:userid`
 }))
-
-
-router.render = (req, res, next) => {
-
-  if(req.method == 'GET') {
-
-      if(req.url.indexOf('/friendships?_expand=user&ownerId=') != -1){//FRIENDS
-            console.log('friendssss')
-            if (res.locals.data.length > 0) {
-
-                res.jsonp(res.locals.data.map(friendship => ({
-                    friendshipId: friendship.id,
-                    ownerId: friendship.ownerId,
-                    ...friendship.user
-                })));
-            }
-
-        } else {
-            res.jsonp(res.locals.data)
-        }
-    } else {
-        res.jsonp(res.locals.data)
-    }
-}
 
 server.use(router)
 
