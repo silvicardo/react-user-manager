9c7a6eafe11aeb2224ef65bb1b9283c630de5b5f
silvicardo <silvicardo@gmail.com>
2021-01-09 22:52:52 +0100

    useUserEditor reducer logic fix and comments

--- src/hooks/useUserEditor.js
+++ src/hooks/useUserEditor.js
@@ -3,39 +3,6 @@ import useUser from "./useUser";
 import {createReducer} from "@reduxjs/toolkit/dist/redux-toolkit.cjs.production.min";
 import useUsersByRelationship from "./useUsersByRelationship";
 
-export const makeFriendEditCreateReducer = () => {
-    const initialState = getInitialUserEditorState()
-    return createReducer(initialState, {
-        SET_STORED_USER_DATA: (state, action) => {
-            for (let stateKey in action.payload){
-                if(state.stored.hasOwnProperty(stateKey)){
-                    state.stored[stateKey] = action.payload[stateKey]
-                }
-            }
-        },
-        SET_USER_TO_BE_FRIEND: (state, action) => {
-            const nextFriendId = action.payload;
-            const alreadyStoredFriend = state.stored.friends.find(friend => friend.id === nextFriendId);
-            state.next.friendsIds.push(nextFriendId);
-            state.next.unrelatedUsersIds.filter(unrelatedId => unrelatedId !== nextFriendId);
-            if(alreadyStoredFriend && alreadyStoredFriend.friendshipId){
-                state.next.removingFriendshipsIds.filter(friendshipId => friendshipId !== alreadyStoredFriend.friendshipId);
-            }
-
-        },
-        SET_USER_TO_BE_UNRELATED: (state, action) => {
-
-                const toDeleteFriendId  = action.payload;
-                const removingFriendshipId = state.stored.friends.find(friend => friend.id === toDeleteFriendId).friendshipId;
-
-                state.next.friendsIds.filter(friendId => friendId !== toDeleteFriendId);
-                state.next.removingFriendshipsIds.push(removingFriendshipId);
-                state.next.unrelatedUsersIds.push(toDeleteFriendId);
-
-        }
-    })
-}
-
 export const getInitialUserEditorState = () => ({
     stored : {
         username: '',
@@ -49,7 +16,46 @@ export const getInitialUserEditorState = () => ({
         removingFriendshipsIds: []//API
     },
 
-})
+});
+
+export const makeFriendEditCreateReducer = () => {
+    
+    const initialState = getInitialUserEditorState()
+
+    //redux toolkit createReducer to take advantage of immer js immutability features
+    return createReducer(initialState, {
+        SET_STORED_USER_DATA: (state, action) => {
+
+            for (let stateKey in action.payload){
+                if(state.stored.hasOwnProperty(stateKey)){
+                    state.stored[stateKey] = action.payload[stateKey]
+                }
+            }
+
+        },
+        SET_USER_TO_BE_FRIEND: (state, action) => {
+
+            const nextFriendId = action.payload;
+            const alreadyStoredFriend = state.stored.friends.find(friend => friend.id === nextFriendId);
+            state.next.friendsIds.push(nextFriendId);
+            state.next.unrelatedUsersIds = state.next.unrelatedUsersIds.filter(unrelatedId => unrelatedId !== nextFriendId);
+            if(alreadyStoredFriend && alreadyStoredFriend.friendshipId){
+                state.next.removingFriendshipsIds.filter(friendshipId => friendshipId !== alreadyStoredFriend.friendshipId);
+            }
+
+        },
+        SET_USER_TO_BE_UNRELATED: (state, action) => {
+
+            const toDeleteFriendId  = action.payload;
+            const removingFriendshipId = state.stored.friends.find(friend => friend.id === toDeleteFriendId).friendshipId;
+
+            state.next.friendsIds = state.next.friendsIds.filter(friendId => friendId !== toDeleteFriendId);
+            state.next.removingFriendshipsIds.push(removingFriendshipId);
+            state.next.unrelatedUsersIds.push(toDeleteFriendId);
+
+        }
+    })
+};
 
 export default function useUserEditor(id = null){
 
